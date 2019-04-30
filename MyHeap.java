7780f940ba03acf74f31e5615156be9980655cc0
Yongho Lee <ylee01@stuy.edu>
2019-04-30 11:03:49 -0400

    Started on a working algorithm

---
+++ MyHeap.java
@@ -0,0 +1,24 @@
+public class MyHeap {
+  private static void pushDown(int[] data, int size, int index) {
+    int var = data[index];
+    while (var < data[2 * index + 1] || var < data[2* index + 2]) {
+      if (var < data[2 * index + 1]) {
+        
+      } else {
+
+      }
+    }
+  }
+
+  private static void pushUp(int[] data, int index) {
+
+  }
+
+  public static void heapify(int[]) {
+
+  }
+
+  public static void heapsort(int[]) {
+
+  }
+}
