b4c09f2ca339cb0d3ed9335fffb7167d2065343b
ylee02 <ylee01@stuy.edu>
2019-05-03 12:42:23 +0900

    Added the two pushes

--- MyHeap.java
+++ MyHeap.java
@@ -1,24 +1,41 @@
+import java.util.*;
 public class MyHeap {
   private static void pushDown(int[] data, int size, int index) {
     int var = data[index];
-    while (var < data[2 * index + 1] || var < data[2* index + 2]) {
-      if (var < data[2 * index + 1]) {
-        
-      } else {
+    while ((2 * index + 1 < size && var < data[2 * index + 1] ) || (2 * index + 2 < size && var < data[2* index + 2])) {
+		  if (var < data[2 * index + 1]) {
+			data[index] = data[2 * index + 1];
+			data[2 * index + 1] = var;
+			index = 2 * index + 1;
+		  } else {
+			data[index] = data[2 * index + 2];
+			data[2 * index + 2] = var;
+			index = 2 * index + 2;
+		  }
 
-      }
     }
   }
 
   private static void pushUp(int[] data, int index) {
+	while (((index + 1) / 2 - 1) >= 0  && data[(index + 1) / 2 - 1] < data[index]) {
+		int temp = data[(index + 1) / 2 - 1];
+		data[(index + 1) / 2 - 1] = data[index];
+		data[index] = temp;
+		index = (index + 1) / 2 - 1;
+	}
+  }
+
+  public static void heapify(int[] data) {
 
   }
 
-  public static void heapify(int[]) {
+  public static void heapsort(int[] data) {
 
   }
-
-  public static void heapsort(int[]) {
-
-  }
+  
+  public static void main(String[] args) {
+	  int[] temp = {4, 2, 6, 1, 1, 9};
+	  pushUp(temp, 5);
+	  System.out.println(Arrays.toString(temp));
+	}
 }
