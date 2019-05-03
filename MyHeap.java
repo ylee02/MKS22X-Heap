9bfc16aef567c69f73498bac72dfab23af622138
ylee02 <ylee01@stuy.edu>
2019-05-03 13:35:33 +0900

    This also took me way longer than it should have

--- MyHeap.java
+++ MyHeap.java
@@ -1,19 +1,29 @@
 import java.util.*;
 public class MyHeap {
   private static void pushDown(int[] data, int size, int index) {
-    int var = data[index];
-    while ((2 * index + 1 < size && var < data[2 * index + 1] ) || (2 * index + 2 < size && var < data[2* index + 2])) {
-		  if (var < data[2 * index + 1]) {
-			data[index] = data[2 * index + 1];
-			data[2 * index + 1] = var;
-			index = 2 * index + 1;
-		  } else {
-			data[index] = data[2 * index + 2];
-			data[2 * index + 2] = var;
-			index = 2 * index + 2;
-		  }
+    int child = 2 * index + 1;
+	while (child < size) {
+		int l = data[child];
+		int max = l;
+		if (child + 1 < size) {
+			int r = data[child + 1];
+			//max = Math.max(r,l);
+			if (r> l){
+				max = r;
+				child++;
+			}
+		}
+		if (data[index] < max) {
+			int temp = data[index];
+			data[index] = data[child];
+			data[child] = temp;
+			index = child;
+		}else return;
+		child = index * 2 + 1;
 
-    }
+    
+	
+	}
   }
 
   private static void pushUp(int[] data, int index) {
@@ -26,16 +36,29 @@ public class MyHeap {
   }
 
   public static void heapify(int[] data) {
-
+	for (int i = data.length -1; i >= 0; i--) {
+		System.out.println("a");
+		pushDown(data, data.length,i);
+	}
   }
 
   public static void heapsort(int[] data) {
-
+	heapify(data);
+	for (int i = data.length; i >1; i--) {
+		System.out.println(Arrays.toString(data));
+		int temp = data[0];
+		data[0] = data[i -1];
+		data[i-1] = temp;
+		pushDown(data,i-1, 0);
+		
+		
+	}
+		
   }
   
   public static void main(String[] args) {
-	  int[] temp = {4, 2, 6, 1, 1, 9};
-	  pushUp(temp, 5);
+	  int[] temp = {7, 2, 3, 4, 6, 9};
+	  heapsort(temp);
 	  System.out.println(Arrays.toString(temp));
 	}
 }
